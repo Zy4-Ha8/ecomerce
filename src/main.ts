240a98700b4713cbb5331757a7fbf0a5e310efab
Zy4-Ha8 <haiderumbra@gmail.com>
2025-10-27 20:19:22 +0300

    fix the bug

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { useContainer } from 'class-validator';
 import helmet from 'helmet';
 import * as morgan from 'morgan';
 import { AllExceptionsFilter } from './filters/http-exception.filter';
-import * as mongoSanitize from 'express-mongo-sanitize';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'debug', 'log', 'verbose'], // Enable all log levels
@@ -15,7 +14,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.use(helmet());
-  app.use(mongoSanitize.default());
+
   app.use(morgan.default('combined'));
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   await app.listen(process.env.PORT ?? 5000, '0.0.0.0');
