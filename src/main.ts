adafe62d198e49424366da98646131c6790922b5
Zy4-Ha8 <haiderumbra@gmail.com>
2025-10-27 20:15:22 +0300

    remove xxs

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
 import helmet from 'helmet';
 import * as morgan from 'morgan';
-import * as xss from 'xss-clean';
 import { AllExceptionsFilter } from './filters/http-exception.filter';
+import * as mongoSanitize from 'express-mongo-sanitize';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'debug', 'log', 'verbose'], // Enable all log levels
@@ -15,8 +15,8 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.use(helmet());
+  app.use(mongoSanitize.default());
   app.use(morgan.default('combined'));
-  app.use(xss.default());
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   await app.listen(process.env.PORT ?? 5000, '0.0.0.0');
 }
