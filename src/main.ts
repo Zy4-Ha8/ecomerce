27cd2321fc9e48bbdf6fa43d9c5458a4e513dd60
Zy4-Ha8 <haiderumbra@gmail.com>
2025-10-28 17:04:42 +0300

    add url to cors

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,12 @@ async function bootstrap() {
   });
 
   app.useGlobalFilters(new AllExceptionsFilter());
-
+  app.enableCors({
+    origin: 'http://localhost:5173', 
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.use(helmet());
 
