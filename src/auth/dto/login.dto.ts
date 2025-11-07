1acbbe855b0fd0d9937d25c96ec94c94fc20d7a9
Zy4-Ha8 <haiderumbra@gmail.com>
2025-11-07 05:21:24 +0300

    LoginDto update

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,7 +1,10 @@
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 export class LoginDto {
+  @IsString()
+  @IsNotEmpty()
   username: string;
 
   @IsString()
+  @IsNotEmpty()
   password: string;
 }
